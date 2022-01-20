419094f1be274de022d89269a89c31f09a5f2dcc
Yee <yee.gamedev@gmail.com>
2022-01-20 17:49:39 -0600

    Control Refactors and Namespace Added (Controls)

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -1,39 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Swapper))]
-public class Control : MonoBehaviour
+namespace TG.Controls
 {
-    float xAxis;
-    float zAxis;
-    bool holdHBtn;
-    bool holdVBtn;
-    bool lastPressedHBtn;
-
-    Mover mover;
-    Swapper swapper;
-
-    private void Awake() { swapper = GetComponent<Swapper>(); }
-    private void Start() { mover = swapper.GetActiveMover(); }
-
-    void Update()
+    [RequireComponent(typeof(Swapper))]
+    public class Control : MonoBehaviour
     {
-        holdHBtn = Input.GetButton("Horizontal");
-        holdVBtn = Input.GetButton("Vertical");
+        float xAxis;
+        float zAxis;
+        bool holdHBtn;
+        bool holdVBtn;
+        bool lastPressedHBtn;
 
-        if (holdHBtn && !holdVBtn || holdVBtn && Input.GetButtonDown("Horizontal")) { lastPressedHBtn = true; }
-        if (!holdHBtn && holdVBtn || holdHBtn && Input.GetButtonDown("Vertical")) { lastPressedHBtn = false; }
+        Mover mover;
+        Swapper swapper;
 
-        xAxis = lastPressedHBtn ? Input.GetAxisRaw("Horizontal") : 0;
-        zAxis = !lastPressedHBtn ? Input.GetAxisRaw("Vertical") : 0;
+        private void Awake() { swapper = GetComponent<Swapper>(); }
+        private void Start() { mover = swapper.GetActiveMover(); }
 
-        if (Input.GetKeyDown(KeyCode.K)) { mover = swapper.Swap(); }
-    }
+        void Update()
+        {
+            CheckLastButtonPressed();
+            SetAxis();
+            ReadSwapInput();
+        }
 
-    private void FixedUpdate()
-    {
-        Debug.Log(mover);
-        mover.Move(xAxis, zAxis);
+        private void CheckLastButtonPressed()
+        {
+            holdHBtn = Input.GetButton("Horizontal");
+            holdVBtn = Input.GetButton("Vertical");
+
+            if (holdHBtn && !holdVBtn || holdVBtn && Input.GetButtonDown("Horizontal")) { lastPressedHBtn = true; }
+            if (!holdHBtn && holdVBtn || holdHBtn && Input.GetButtonDown("Vertical")) { lastPressedHBtn = false; }
+        }
+
+        private void SetAxis()
+        {
+            xAxis = lastPressedHBtn ? Input.GetAxisRaw("Horizontal") : 0;
+            zAxis = !lastPressedHBtn ? Input.GetAxisRaw("Vertical") : 0;
+        }
+
+        //TODO Add fancy animation
+        private void ReadSwapInput() { if (Input.GetKeyDown(KeyCode.K)) { mover = swapper.Swap(); } }
+
+        private void FixedUpdate() { mover.Move(xAxis, zAxis); }
     }
 }
