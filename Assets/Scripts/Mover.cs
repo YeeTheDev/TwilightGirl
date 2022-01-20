d00c6e4c8a96276c54caa4ce5f5c79f87b07b7f7
Yee <yee.gamedev@gmail.com>
2022-01-20 17:58:44 -0600

    Mover Refactor

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,15 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Mover : MonoBehaviour
+namespace TG.Movement
 {
-    [SerializeField] float speed = 3f;
+    public abstract class Mover : MonoBehaviour
+    {
+        [SerializeField] float speed = 3f;
 
-    public float Speed { get => speed; }
+        public float Speed { get => speed; }
 
-    public abstract bool IsRigidbodyEnabled();
+        public abstract bool IsRigidbodyEnabled();
 
-    public abstract void Move(float xAxis, float zAxis);
-    public abstract void SetBehaviorOnMovement(bool use3DMovement);
+        public abstract void Move(float xAxis, float zAxis);
+        public abstract void SetBehaviorOnMovement(bool use3DMovement);
+    }
 }
