e1bde35fb39f867851cdb308e175be6c5a7afae2
Yee <yee.gamedev@gmail.com>
2022-01-20 17:59:00 -0600

    Mover2D and Mover3D Refactor

--- Assets/Scripts/Mover2D.cs
+++ Assets/Scripts/Mover2D.cs
@@ -1,32 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class Mover2D : Mover
+namespace TG.Movement
 {
-    Rigidbody2D rb;
-
-    private void Awake()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Mover2D : Mover
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
+        Rigidbody2D rb;
 
-    public override bool IsRigidbodyEnabled() { return !rb.isKinematic; }
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
 
-    public override void Move(float xAxis, float zAxis)
-    {
-        Debug.Log("Moving 2D thingy");
-        Vector2 moveDirection = Vector2.zero;
-        moveDirection.x = xAxis * Speed;
-        moveDirection.y = rb.velocity.y;
+        public override bool IsRigidbodyEnabled() { return !rb.isKinematic; }
 
-        rb.velocity = moveDirection;
-    }
+        public override void Move(float xAxis, float zAxis) { rb.velocity = CalculateDirection(xAxis); }
 
-    public override void SetBehaviorOnMovement(bool use3DMovement)
-    {
-        rb.bodyType = use3DMovement ? RigidbodyType2D.Kinematic : RigidbodyType2D.Dynamic;
-        rb.velocity = Vector3.zero;
+        private Vector2 CalculateDirection(float xAxis)
+        {
+            Vector2 moveDirection = Vector2.zero;
+            moveDirection.x = xAxis * Speed;
+            moveDirection.y = rb.velocity.y;
+
+            return moveDirection;
+        }
+
+        public override void SetBehaviorOnMovement(bool use3DMovement)
+        {
+            rb.bodyType = use3DMovement ? RigidbodyType2D.Kinematic : RigidbodyType2D.Dynamic;
+            rb.velocity = Vector3.zero;
+        }
     }
 }
--- Assets/Scripts/Mover3D.cs
+++ Assets/Scripts/Mover3D.cs
@@ -1,32 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class Mover3D : Mover
+namespace TG.Movement
 {
-    Rigidbody rb;
-
-    private void Awake()
+    [RequireComponent(typeof(Rigidbody))]
+    public class Mover3D : Mover
     {
-        rb = GetComponent<Rigidbody>();
-    }
+        Rigidbody rb;
 
-    public override bool IsRigidbodyEnabled() { return !rb.isKinematic; }
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
 
-    public override void Move(float xAxis, float zAxis)
-    {
-        Debug.Log("3D thingy moving");
-        Vector3 moveDirection = Vector3.one * rb.velocity.y;
-        moveDirection.x = xAxis * Speed;
-        moveDirection.z = zAxis * Speed;
+        public override bool IsRigidbodyEnabled() { return !rb.isKinematic; }
 
-        rb.velocity = moveDirection;
-    }
+        public override void Move(float xAxis, float zAxis) { rb.velocity = CalculateDirection(xAxis, zAxis); }
 
-    public override void SetBehaviorOnMovement(bool use3DMovement)
-    {
-        rb.isKinematic = !use3DMovement;
-        rb.velocity = Vector3.zero;
+        private Vector3 CalculateDirection(float xAxis, float zAxis)
+        {
+            Vector3 moveDirection = Vector3.one * rb.velocity.y;
+            moveDirection.x = xAxis * Speed;
+            moveDirection.z = zAxis * Speed;
+
+            return moveDirection;
+        }
+
+        public override void SetBehaviorOnMovement(bool use3DMovement)
+        {
+            rb.isKinematic = !use3DMovement;
+            rb.velocity = Vector3.zero;
+        }
     }
 }
