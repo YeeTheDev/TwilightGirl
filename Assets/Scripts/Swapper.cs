1e84e1f9c30c5b749d842f0cee4fab3da193a163
Yee <yee.gamedev@gmail.com>
2022-01-20 17:07:00 -0600

    Swapper Script Created

---
+++ Assets/Scripts/Swapper.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swapper : MonoBehaviour
+{
+    [SerializeField] Mover3D mover3D = null;
+    [SerializeField] Mover2D mover2D = null;
+    [SerializeField] Transform playerParent = null;
+
+    bool in3D;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Swap();
+        }
+    }
+
+    private void Swap()
+    {
+        in3D = !in3D;
+
+        mover3D.DisableMovement(in3D);
+        mover2D.DisableMovement(in3D);
+
+        mover3D.transform.parent = null;
+        mover2D.transform.parent = null;
+        mover3D.transform.parent = in3D ? playerParent : mover2D.transform;
+        mover2D.transform.parent = in3D ? mover3D.transform : playerParent;
+    }
+}
