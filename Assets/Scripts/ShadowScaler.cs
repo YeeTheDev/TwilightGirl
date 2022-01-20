87d724d9ffbe9ccfd268d3740b19db7e0ff2516c
Yee <yee.gamedev@gmail.com>
2022-01-20 16:37:21 -0600

    ShadowScaler Removed Namespaces Added ShadowControl Namespace

--- Assets/Scripts/ShadowScaler.cs
+++ Assets/Scripts/ShadowScaler.cs
@@ -1,35 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ShadowScaler : MonoBehaviour
+namespace TG.ShadowControl
 {
-    [SerializeField] float shadowMaxSize = 2f;
-    [SerializeField] float maxDistance = 9f;
-    [SerializeField] Transform shadow = null;
-    [SerializeField] Transform model = null;
-
-    float shadowZPosition;
-
-    private void Awake() { shadowZPosition = shadow.position.z; }
-
-    private void Update()
+    public class ShadowScaler : MonoBehaviour
     {
-        ClampShadowZPosition();
-        ScaleShadowByDistance();
-    }
+        [SerializeField] float shadowMaxSize = 2f;
+        [SerializeField] float maxDistance = 9f;
+        [SerializeField] Transform shadow = null;
+        [SerializeField] Transform model = null;
 
-    private void ClampShadowZPosition()
-    {
-        Vector3 zClampedPosition = shadow.position;
-        zClampedPosition.z = shadowZPosition;
-        shadow.position = zClampedPosition;
-    }
+        float shadowZPosition;
 
-    private void ScaleShadowByDistance()
-    {
-        float lerpT = (shadow.position.z - model.position.z - model.localScale.z / 2) / 9;
-        float scaleByDistance = Mathf.Lerp(1, shadowMaxSize, lerpT);
-        shadow.localScale = Vector3.one * scaleByDistance;
+        private void Awake() { shadowZPosition = shadow.position.z; }
+
+        private void Update()
+        {
+            ClampShadowZPosition();
+            ScaleShadowByDistance();
+        }
+
+        private void ClampShadowZPosition()
+        {
+            Vector3 zClampedPosition = shadow.position;
+            zClampedPosition.z = shadowZPosition;
+            shadow.position = zClampedPosition;
+        }
+
+        private void ScaleShadowByDistance()
+        {
+            float lerpT = (shadow.position.z - model.position.z - model.localScale.z / 2) / 9;
+            float scaleByDistance = Mathf.Lerp(1, shadowMaxSize, lerpT);
+            shadow.localScale = Vector3.one * scaleByDistance;
+        }
     }
 }
