d4b03048e4db3d1c0e99c69240f98b5f3f6bcdb5
Yee <yee.gamedev@gmail.com>
2022-01-20 17:45:31 -0600

    Mover Abstract Script Created

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mover : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+
+    public float Speed { get => speed; }
+
+    public abstract bool IsRigidbodyEnabled();
+
+    public abstract void Move(float xAxis, float zAxis);
+    public abstract void SetBehaviorOnMovement(bool use3DMovement);
+}
