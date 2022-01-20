a62f7da63811bbbf67cd216b2e246f375929c76c
Yee <yee.gamedev@gmail.com>
2022-01-20 16:46:03 -0600

    3D Player Can Move

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+
+    bool holdHBtn;
+    bool holdVBtn;
+    bool lastPressedHBtn;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        holdHBtn = Input.GetButton("Horizontal");
+        holdVBtn = Input.GetButton("Vertical");
+
+        if (holdHBtn && !holdVBtn || holdVBtn && Input.GetButtonDown("Horizontal")) { lastPressedHBtn = true; }
+        if (!holdHBtn && holdVBtn || holdHBtn && Input.GetButtonDown("Vertical")) { lastPressedHBtn = false; }
+
+        Vector3 moveDirection = Vector3.zero * rb.velocity.y;
+        moveDirection.x = lastPressedHBtn ? Input.GetAxisRaw("Horizontal") * speed : 0;
+        moveDirection.z = !lastPressedHBtn ? Input.GetAxisRaw("Vertical") * speed : 0;
+
+        rb.velocity = moveDirection;
+    }
+}
