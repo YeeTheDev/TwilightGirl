15a07ce8fa0032591f6442a4aa54dfaf217811ec
Yee <yee.gamedev@gmail.com>
2022-01-21 17:20:44 -0600

    Swapper Changed to PlaneSwapper

---
+++ Assets/Scripts/PlaneSwapper.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using TG.Movement;
+
+namespace TG.Abilities
+{
+    public class PlaneSwapper : MonoBehaviour
+    {
+        [SerializeField] Collider collider3D = null;
+        [SerializeField] Collider colliderShadow = null;
+
+        public bool InShadowRealm { get => colliderShadow.enabled; }
+
+        public void SwapPlane()
+        {
+            collider3D.enabled = !collider3D.enabled;
+            colliderShadow.enabled = !colliderShadow.enabled;
+        }
+    }
+}
