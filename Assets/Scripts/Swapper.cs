a46c44ac2e9d4ff71a7b82c98ebd2810800b7674
Yee <yee.gamedev@gmail.com>
2022-01-20 18:00:47 -0600

    Swapper Refactor

--- Assets/Scripts/Swapper.cs
+++ Assets/Scripts/Swapper.cs
@@ -1,33 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TG.Movement;
 
-public class Swapper : MonoBehaviour
+namespace TG.Abilities
 {
-    [SerializeField] Mover3D mover3D = null;
-    [SerializeField] Mover2D mover2D = null;
-    [SerializeField] Transform playerParent = null;
-
-    bool use3DMovement = true;
-
-    public Mover GetActiveMover()
+    public class Swapper : MonoBehaviour
     {
-        if (mover3D.IsRigidbodyEnabled()) { return mover3D; }
-        return mover2D;
-    }
+        [SerializeField] Mover3D mover3D = null;
+        [SerializeField] Mover2D mover2D = null;
+        [SerializeField] Transform playerParent = null;
 
-    public Mover Swap()
-    {
-        use3DMovement = !use3DMovement;
+        bool use3DMovement = true;
 
-        mover3D.SetBehaviorOnMovement(use3DMovement);
-        mover2D.SetBehaviorOnMovement(use3DMovement);
+        public Mover GetActiveMover()
+        {
+            if (mover3D.IsRigidbodyEnabled()) { return mover3D; }
+            return mover2D;
+        }
 
-        mover3D.transform.parent = null;
-        mover2D.transform.parent = null;
-        mover3D.transform.parent = use3DMovement ? playerParent : mover2D.transform;
-        mover2D.transform.parent = use3DMovement ? mover3D.transform : playerParent;
+        public void Swap()
+        {
+            use3DMovement = !use3DMovement;
 
-        return GetActiveMover();
+            SetCorrectMoverBehavior();
+            SetMoverParent();
+        }
+
+        private void SetCorrectMoverBehavior()
+        {
+            mover3D.SetBehaviorOnMovement(use3DMovement);
+            mover2D.SetBehaviorOnMovement(use3DMovement);
+        }
+
+        private void SetMoverParent()
+        {
+            mover3D.transform.parent = null;
+            mover2D.transform.parent = null;
+            mover3D.transform.parent = use3DMovement ? playerParent : mover2D.transform;
+            mover2D.transform.parent = use3DMovement ? mover3D.transform : playerParent;
+        }
     }
 }
