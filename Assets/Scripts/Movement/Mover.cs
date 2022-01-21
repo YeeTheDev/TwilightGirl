4b0fb2e5bd9896c3dda085cae7a1094f740a3005
Yee <yee.gamedev@gmail.com>
2022-01-21 11:18:10 -0600

    Mover Added Code To Jump

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -5,12 +5,15 @@ namespace TG.Movement
     public abstract class Mover : MonoBehaviour
     {
         [SerializeField] float speed = 3f;
+        [SerializeField] float jumpForce = 50f;
 
         public float Speed { get => speed; }
+        public float JumpForce { get => jumpForce; }
 
         public abstract bool IsRigidbodyEnabled();
 
         public abstract void Move(float xAxis, float zAxis);
         public abstract void SetBehaviorOnMovement(bool use3DMovement);
+        public abstract void Jump(bool halt);
     }
 }
