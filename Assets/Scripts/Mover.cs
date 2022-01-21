c925b52d37a0e4cd242588083bc884847af87f21
Yee <yee.gamedev@gmail.com>
2022-01-21 17:21:17 -0600

    Mover3D Changed to Mover

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,36 +3,39 @@ using UnityEngine;
 namespace TG.Movement
 {
     [RequireComponent(typeof(Rigidbody))]
-    public class Mover3D : Mover
+    public class Mover : MonoBehaviour
     {
+        [SerializeField] float speed = 3f;
+        [SerializeField] float jumpForce = 6f;
+
         Rigidbody rb;
 
         private void Awake() { rb = GetComponent<Rigidbody>(); }
 
-        public override bool IsRigidbodyEnabled() { return !rb.isKinematic; }
-        public override void Move(float xAxis, float zAxis) { rb.velocity = CalculateDirection(xAxis, zAxis); }
+        public bool IsRigidbodyEnabled() { return !rb.isKinematic; }
+        public void Move(float xAxis, float zAxis) { rb.velocity = CalculateDirection(xAxis, zAxis); }
 
         private Vector3 CalculateDirection(float xAxis, float zAxis)
         {
             Vector3 moveDirection = Vector3.one * rb.velocity.y;
-            moveDirection.x = xAxis * Speed;
-            moveDirection.z = zAxis * Speed;
+            moveDirection.x = xAxis * speed;
+            moveDirection.z = zAxis * speed;
 
             return moveDirection;
         }
 
-        public override void SetBehaviorOnMovement(bool use3DMovement)
+        public void SetBehaviorOnMovement(bool use3DMovement)
         {
             rb.isKinematic = !use3DMovement;
             rb.velocity = Vector3.zero;
         }
 
-        public override void Jump(bool halt) { rb.velocity = CalculateJumpForce(halt); }
+        public void Jump(bool halt) { rb.velocity = CalculateJumpForce(halt); }
 
         private Vector3 CalculateJumpForce(bool halt)
         {
             Vector3 jumpVelocity = rb.velocity;
-            jumpVelocity.y = halt ? rb.velocity.y > 0 ? jumpVelocity.y * 0.5f : rb.velocity.y : JumpForce;
+            jumpVelocity.y = halt ? rb.velocity.y > 0 ? jumpVelocity.y * 0.5f : rb.velocity.y : jumpForce;
 
             return jumpVelocity;
         }
