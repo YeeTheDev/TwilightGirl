fbcb93f0a25a8b465cc720f18086afbee3f9ad5e
Yee <yee.gamedev@gmail.com>
2022-01-21 12:55:00 -0600

    PlayerCollisioner Script Created

---
+++ Assets/Scripts/PlayerCollisioner.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollisioner : MonoBehaviour
+{
+    [SerializeField] Transform respawnPoint = null;
+    [SerializeField] int levelToLoad = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Damager"))
+        {
+            transform.position = respawnPoint.position;
+        }
+
+        if (collision.transform.CompareTag("Goal"))
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
