a9c044553c8f49f487a85c4c3a946759e52ae358
Yee <yee.gamedev@gmail.com>
2022-01-22 12:49:22 -0600

    Namespace Changes

--- Assets/Scripts/ShadowTerrainCreator.cs
+++ Assets/Scripts/ShadowTerrainCreator.cs
@@ -1,44 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ShadowTerrainCreator : MonoBehaviour
+namespace TG.Core
 {
-    [SerializeField] GameObject terrain = null;
-    [SerializeField] float depth = 7;
-
-    private void Awake() { CreateShadowTerrain(); }
-
-    private void CreateShadowTerrain()
+    public class ShadowTerrainCreator : MonoBehaviour
     {
-        GameObject shadowColliders = Instantiate(terrain, transform);
-        shadowColliders.name = "Shadow Colliders";
+        [SerializeField] GameObject terrain = null;
+        [SerializeField] float depth = 7;
 
-        foreach (Transform child in shadowColliders.transform)
+        private void Awake() { CreateShadowTerrain(); }
+
+        private void CreateShadowTerrain()
         {
-            SetChildPosition(child);
-            SetChildScale(child);
-            DisableChildRenderer(child);
+            GameObject shadowColliders = Instantiate(terrain, transform);
+            shadowColliders.name = "Shadow Colliders";
+
+            foreach (Transform child in shadowColliders.transform)
+            {
+                SetChildPosition(child);
+                SetChildScale(child);
+                DisableChildRenderer(child);
+            }
         }
-    }
 
-    private void SetChildPosition(Transform child)
-    {
-        Vector3 newPosition = child.position;
-        newPosition.z = depth;
-        child.position = newPosition;
-    }
+        private void SetChildPosition(Transform child)
+        {
+            Vector3 newPosition = child.position;
+            newPosition.z = depth;
+            child.position = newPosition;
+        }
 
-    private static void DisableChildRenderer(Transform child)
-    {
-        MeshRenderer meshRenderer = child.GetComponent<MeshRenderer>();
-        meshRenderer.enabled = false;
-    }
+        private static void DisableChildRenderer(Transform child)
+        {
+            MeshRenderer meshRenderer = child.GetComponent<MeshRenderer>();
+            meshRenderer.enabled = false;
+        }
 
-    private void SetChildScale(Transform child)
-    {
-        Vector3 normalizedScale = child.localScale;
-        normalizedScale.z = 1;
-        child.localScale = normalizedScale;
+        private void SetChildScale(Transform child)
+        {
+            Vector3 normalizedScale = child.localScale;
+            normalizedScale.z = 1;
+            child.localScale = normalizedScale;
+        }
     }
 }
