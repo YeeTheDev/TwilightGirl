9467fe1cbe31af5b63b584b322031262fe3c66b0
Yee <yee.gamedev@gmail.com>
2022-01-22 12:36:51 -0600

    PlaneSwapper Won't Work If Touching a Shadow

--- Assets/Scripts/PlaneSwapper.cs
+++ Assets/Scripts/PlaneSwapper.cs
@@ -4,6 +4,7 @@ namespace TG.Abilities
 {
     public class PlaneSwapper : MonoBehaviour
     {
+        [SerializeField] float shadowCheckerRadius = 0.45f;
         [SerializeField] Collider collider3D = null;
         [SerializeField] Collider colliderShadow = null;
 
@@ -11,8 +12,23 @@ namespace TG.Abilities
 
         public void SwapPlane()
         {
+            if (CheckIfShadowsTouching()) { return; }
+
             collider3D.enabled = !collider3D.enabled;
             colliderShadow.enabled = !colliderShadow.enabled;
+        }
+
+        public bool CheckIfShadowsTouching()
+        {
+            BoxCollider box = colliderShadow as BoxCollider;
+            if (box != null && !colliderShadow.enabled)
+            {
+                float radius = shadowCheckerRadius * colliderShadow.transform.localScale.x;
+                Vector3 checkerCenter = colliderShadow.transform.position + box.center * colliderShadow.transform.localScale.x;
+                if (Physics.CheckSphere(checkerCenter, radius)) { return true; }
+            }
+
+            return false;
         }
     }
 }
