e6d23acd52757c0b33db89a06d9d6d79bede8780
Yee <yee.gamedev@gmail.com>
2022-01-22 13:07:35 -0600

    Created Collisioner Script

---
+++ Assets/Scripts/Collisioner.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collisioner : MonoBehaviour
+{
+    [SerializeField] string goalTag = "Goal";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(goalTag))
+        {
+            int sceneToLoad = (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
