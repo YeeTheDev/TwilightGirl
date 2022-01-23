7feada9f0fc792919bae1158ada6adde48a39674
Yee <yee.gamedev@gmail.com>
2022-01-22 19:26:37 -0600

    Shadow Scaler Now Sets The Collider's Z Position

--- Assets/Scripts/ShadowScaler.cs
+++ Assets/Scripts/ShadowScaler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TG.Core;
 
 namespace TG.ShadowControl
 {
@@ -9,23 +10,38 @@ namespace TG.ShadowControl
         [SerializeField] float wallDistance = 5f;
         [SerializeField] Transform shadow = null;
         [SerializeField] Transform shadowCollider = null;
+        [SerializeField] string terrainTag = "Terrain";
 
-        float shadowZPosition;
+        float stuckZPosition;
+        ShadowTerrainCreator terrainCreator;
 
         public float ShadowXScale { get => shadowCollider.localScale.x; }
 
-        private void Awake() { shadowZPosition = shadowCollider.position.z; }
+        private void Awake()
+        {
+            terrainCreator = GameObject.FindGameObjectWithTag(terrainTag).GetComponent<ShadowTerrainCreator>();
+
+            SetInitialZPosition();
+        }
+
+        private void SetInitialZPosition()
+        {
+            Vector3 shadowColliderStartPosition = transform.position;
+            shadowColliderStartPosition.z = terrainCreator.GetDepth;
+            shadowCollider.position = shadowColliderStartPosition;
+            stuckZPosition = shadowCollider.position.z;
+        }
 
         private void Update()
         {
-            ClampColliderZPosition();
+            StuckColliderZPosition();
             ScaleShadowByDistance();
         }
 
-        private void ClampColliderZPosition()
+        private void StuckColliderZPosition()
         {
             Vector3 zClampedPosition = shadowCollider.position;
-            zClampedPosition.z = shadowZPosition;
+            zClampedPosition.z = stuckZPosition;
             shadowCollider.position = zClampedPosition;
         }
 
