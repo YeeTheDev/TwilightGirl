cc9e5c964acb8c7477921cedf06125dc28ec6797
Yee <yee.gamedev@gmail.com>
2022-01-26 19:34:19 -0600

    Animater Now Handles Turn Animation

--- Assets/Scripts/Animater.cs
+++ Assets/Scripts/Animater.cs
@@ -15,6 +15,8 @@ namespace TG.Animations
         [SerializeField] string yVelocityParameter = "Y_Velocity";
         [SerializeField] string swapParameter = "Swap";
         [SerializeField] string groundedParameter = "Grounded";
+        [SerializeField] string xAxisParameter = "XAxis";
+        [SerializeField] string zAxisParameter = "ZAxis";
 
         float timer;
 
@@ -40,6 +42,12 @@ namespace TG.Animations
             shadowAnimator.SetFloat(yVelocityParameter, yVelocity);
         }
 
+        public void RotateCharacter(float xAxis, float zAxis)
+        {
+            if (!Mathf.Approximately(xAxis, 0)) { RotateXAxis(xAxis); }
+            else if (!Mathf.Approximately(zAxis, 0)) { RotateZAxis(zAxis); }
+        }
+
         private void SetTrigger(string parameter)
         {
             meshAnimator.SetTrigger(parameter);
@@ -50,6 +58,24 @@ namespace TG.Animations
         {
             meshAnimator.SetBool(parameter, state);
             shadowAnimator.SetBool(parameter, state);
+        }
+
+        private void RotateXAxis(float xAxis)
+        {
+            meshAnimator.SetFloat(xAxisParameter, xAxis);
+            meshAnimator.SetFloat(zAxisParameter, 0);
+
+            shadowAnimator.SetFloat(xAxisParameter, xAxis);
+            shadowAnimator.SetFloat(zAxisParameter, 0);
+        }
+
+        private void RotateZAxis(float zAxis)
+        {
+            meshAnimator.SetFloat(xAxisParameter, 0);
+            meshAnimator.SetFloat(zAxisParameter, zAxis);
+
+            shadowAnimator.SetFloat(xAxisParameter, 0);
+            shadowAnimator.SetFloat(zAxisParameter, zAxis);
         }
     }
 }
