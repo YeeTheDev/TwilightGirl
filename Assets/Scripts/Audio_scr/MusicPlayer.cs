a5623f8493cbd0edf0622897731d1322d0843b57
Yee <yee.gamedev@gmail.com>
2022-01-29 00:33:55 -0600

    Music Added

---
+++ Assets/Scripts/Audio_scr/MusicPlayer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace TG.Audio
+{
+    public class MusicPlayer : MonoBehaviour
+    {
+        public static MusicPlayer singleton;
+
+        private void Awake() { SetUpSingleton(); }
+
+        private void SetUpSingleton()
+        {
+            if (singleton == null) { singleton = this; }
+            else { Destroy(gameObject); }
+        }
+    }
+}
