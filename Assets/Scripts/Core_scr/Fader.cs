24135da950ee28f095574b33573e600af75db345
Yee <yee.gamedev@gmail.com>
2022-01-29 10:59:44 -0600

    Fades and Loads

---
+++ Assets/Scripts/Core_scr/Fader.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TG.Core;
+
+namespace TG.UserInterface
+{
+    [RequireComponent(typeof(Animator))] 
+    public class Fader : MonoBehaviour
+    {
+        [SerializeField] Image fadeImage = null;
+        [SerializeField] string fadeParameter = "Fade";
+        [SerializeField] AnimationClip fadeAnimationClip = null;
+
+        Animator animator;
+        SceneLoader sceneLoader;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            sceneLoader = FindObjectOfType<SceneLoader>();
+        }
+
+        public void FadeAndLoad(int sceneToLoad)
+        {
+            animator.SetTrigger(fadeParameter);
+            sceneLoader.StartLoading(fadeAnimationClip.length, sceneToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/Core_scr/SceneLoader.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TG.Core
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        [SerializeField] float extraTimeToLoad = 0.1f;
+
+        int sceneToLoad;
+
+        public void StartLoading(float timeBeforeLoad, int sceneIndex)
+        {
+            sceneToLoad = sceneIndex;
+            Invoke("LoadScene", timeBeforeLoad + extraTimeToLoad);
+        }
+
+        public void LoadScene() { SceneManager.LoadScene(sceneToLoad); }
+    }
+}
